42a8c11156bf5d3e01dd440b798c99742acb5db9
joannez8 <joannez8@nycstudents.net>
2025-09-26 00:09:32 -0400

    Initial commit.

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        DogWalkCompany c = new DogWalkCompany();
+        DogWalker w = new DogWalker(3,c);
+        System.out.println(c);
+        int hour = c.addDogs();
+        /** for (int i = 7; i < 11; i++)
+            c.addDogs(); */
+        System.out.println(c);
+        System.out.println(w.walkDogs(hour) + " dogs walked.");
+        System.out.println(c.numAvailableDogs(hour) + " dogs remaining.");
+        System.out.println("$" + w.dogWalkShift(7, 10) + ".00 earned.");
+    }
+}
